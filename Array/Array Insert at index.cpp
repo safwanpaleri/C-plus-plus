dbe42d43a53d312ccdc30166e1250f7fc2cdf691
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 12:26:40 +0530

    Update Array Insert at index.cpp

--- Array/Array Insert at index.cpp
+++ Array/Array Insert at index.cpp
@@ -1,20 +1,27 @@
+//Insert an user-input element in a user given index in a given array;
 #include <bits/stdc++.h>
 using namespace std;
 main()
 {
 	while(true)
-	{
-		int b[]={1,2,3,4,5,0};
-		int a,in;
+	{	//initialising 
+		int b[]={1,2,3,4,5,0},a,in;
+		
+		//taking inputs from user
 		cout<<"enter no:";
 		cin>>a;
 		cout<<"enter index";
 		cin>>in;
+		
+		//traversing through array and adding the element
+		// in the given index.
 		for(int i=6;i>=in;i--)
 			if(i==in)
 				b[i]=a;
 			else
 				b[i]=b[i-1];
+		
+		//printing the array on the screen
 		for(int j=0;j<6;j++)
 			cout<<b[j]<<" ";
 		cout<<endl;
