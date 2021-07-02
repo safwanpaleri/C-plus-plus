4a1813d31dd59398140f5939e915df81ea0f28e1
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 12:23:22 +0530

    Update Array Insert at end.cpp

--- Array/Array Insert at end.cpp
+++ Array/Array Insert at end.cpp
@@ -1,22 +1,17 @@
+//Insert the user-input element at back of an given array.
 #include <bits/stdc++.h>
 using namespace std;
 main()
 {
-	
-	int b;
 	while(true)
 	{
-		int a[]={1,2,3,4,5};
+		int a[]={1,2,3,4,5},b;
 		cout<<"Enter No to insert:";
 		cin>>b;
 		int n = 5;
 		a[n+1];
 		a[n]=b;
 		for(int i=0;i<n+1;i++)
-		{
 			cout<<a[i]<<" ";
-		}
-
-		//cout<<a[n]<<endl;
 	}
 }
