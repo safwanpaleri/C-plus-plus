1a0d3517e96add7d060d749b0c128f25487f46c6
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 13:07:33 +0530

    Update Maximum index.cpp

--- Array/Maximum index.cpp
+++ Array/Maximum index.cpp
@@ -1,22 +1,23 @@
+//Fnd the maximum j – i such that arr[j] > arr[i]
+
 #include <bits/stdc++.h>
 using namespace std;
 main()
 {
+	//initialization
 	int a[]= {70, 83, 82, 39, 81};
+	//you can find the size of an array using this method
 	int n= sizeof(a)/sizeof(a[0]);
 	int max=1;
+	
+	//traversing through array while checking the condition of arr[j] > arr[i]
+	//whenever found compare the abs(j-i) value with max and update
 	for(int i=0;i<n;i++)
-	{
 		for(int j=n-1;j>i;j--)
-		{
 			if(a[i]<=a[j])
-			{
-				cout<<abs(i-j)<<" ";
 				if(max<abs(i-j))
 					max=abs(i-j);
-			}
-		}
-		cout<<endl;
-	}
+	
+	//print the max to the user.
 	cout<<max;
 }
