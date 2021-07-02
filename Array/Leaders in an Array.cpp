0c76e801b200710b9706a8293347e8dfd23953b8
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 12:51:48 +0530

    Update Leaders in an Array.cpp

--- Array/Leaders in an Array.cpp
+++ Array/Leaders in an Array.cpp
@@ -1,10 +1,18 @@
+//print all the leaders of the array
+//An element is leader if it is greater than all the elements to its right side
+
 #include <bits/stdc++.h>
 using namespace std;
 main()
 {
-	int n=6;
-	int arr[]={16,17,4,3,5,2};
+	//initialization
+	int n=7;
+	int arr[]={15,16,3,2,4,1,0};
 	int max = arr[n-1];
+	
+	//traversing from rear of the array and checking
+	//if the current element is greater than max element
+	//if yes print the element and change max
 	for(int i=n-1;i>0;i--)
 	{
 		if(max<arr[i])
