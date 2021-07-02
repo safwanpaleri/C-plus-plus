713afa363d1cefb0b7ab676b3f1b548dbba93c7f
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 13:40:08 +0530

    Update Minimum adjacent difference in a circular array.cpp

--- Array/Minimum adjacent difference in a circular array.cpp
+++ Array/Minimum adjacent difference in a circular array.cpp
@@ -1,21 +1,32 @@
+//Find the minimum of difference between two adjacent elements in an array
+
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
+	//initialization
 	int n=26;
 	int arr[]={1,21,5,47,71,0,36,22,64,46,18,11,73,13,57,68,8,45,99,78,3,33,17,13,30,26};
 	int diff=0;
+	
+	//initializing first result to be difference between first and second element
 	int res=abs(arr[0]-arr[1]);
+
+	//transversing through array and finding the differences between the two arrays
 	for(int i=0;i<n;i++)
 	{
+		//if we are at the last element, we find the difference between the last and the first 
+		//element of that array, 
+		//else to the adjacent element
 		if(i==n-1)
 			diff=abs(arr[i]-arr[0]);
 		else
 			diff=abs(arr[i]-arr[i+1]);
-		if(diff>0)
-			res=min(res,diff);
-		cout<<diff<<" ";
+		
+		//comparing current difference and previous minimum and
+		//changing the result accordingly
+		res=min(res,diff);
 	}
-	cout<<endl;
+	//printing the screen to user
 	cout<<res;
 }
