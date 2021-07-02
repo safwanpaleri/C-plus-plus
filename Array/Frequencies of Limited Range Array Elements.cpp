b501c3299f9fc6ff03eaf2caf76a8fe7669f964a
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 12:44:03 +0530

    Update Frequencies of Limited Range Array Elements.cpp

--- Array/Frequencies of Limited Range Array Elements.cpp
+++ Array/Frequencies of Limited Range Array Elements.cpp
@@ -1,19 +1,28 @@
+//Find occurences of each element in array and print the count.
+//All the elements is less than a given constant(p in this initialization)
+
 #include <bits/stdc++.h>
 using namespace std;
 main()
 {
+	//initialization
 	int n=5,p=5;
 	int arr[]={2,3,2,3,5};
+	
+	//traversing from 1 to p as the elements in the array is less than p.
 	for(int i=1;i<=p;i++)
 	{
+		//initialization of count which also reset in every iteration
 		int count=0;
+		
+		//traversing through array and checking the elements
 		for(int j=0;j<n;j++)
 		{
+			//increasing count if any match found
 			if(i==arr[j])
-			{
 				count++;
-			}
 		}
-		cout<<count<<" ";
+		//printing the result to the screen
+		cout<<"element "<<i<<"found "<<count<<"times "<<endl;
 	}
 }
