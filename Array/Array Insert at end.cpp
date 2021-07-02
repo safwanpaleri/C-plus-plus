7a32670992ada9afe5141d9a01d0c8fb5dc0a2aa
safwanpaleri <safwanpaleri@gmail.com>
2021-07-02 12:29:22 +0530

    Update Array Insert at end.cpp

--- Array/Array Insert at end.cpp
+++ Array/Array Insert at end.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 main()
 {
+	//runs the programe for infinite times
 	while(true)
 	{
+		//initializing array and required items.
 		int a[]={1,2,3,4,5},b;
+		
+		//Taking inputs from user
 		cout<<"Enter No to insert:";
 		cin>>b;
+		
+		//increasing the size of the array.
+		// adding the element at the rear of the array
 		int n = 5;
 		a[n+1];
 		a[n]=b;
+		
+		//printing the result array to the screen
 		for(int i=0;i<n+1;i++)
 			cout<<a[i]<<" ";
 	}
