f0b9c15e4f9e33d4fe7928c36573208c9b89d88b
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 13:21:06 +0530

    Update Longest consecutive 1.cpp

--- Calculations Using Bits/Longest consecutive 1.cpp
+++ Calculations Using Bits/Longest consecutive 1.cpp
@@ -1,23 +1,32 @@
+//Finding the longest consecutive occurence of 1 in a binary array
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
-	int n=0;
-	while(true)
+	//initializations
+	int n=0,m=0,c=0;
+	
+	//taking inputs from user
+	cout<<"enter n:";
+	cin>>n;
+	
+	//while the number is still greater then zero
+	while(n>0)
 	{
-		cout<<"enter n:";
-		cin>>n;
-		int c=0;
-		vector<int> arr;
-		while(n>0)
-		{
-			arr.push_back(c);
-			if(n&1)
-				c++;
-			else
-				c=0;
-			n=n>>1;
-		}
-		cout<<*max_element(arr.begin(), arr.end())<<endl;
+		//compare the last bit whether is zero or one
+		//if one increment the count variable by one
+		//else reset count variable into 0
+		if(n&1)
+			c++;
+		else
+			c=0;
+		
+		//update the max value of count if changes
+		m=max(m,c);
+		
+		//right shift the number by one
+		n=n>>1;
 	}
+	//print the maximum value of c occured while running the loop
+	cout<<m<<endl;
 }
