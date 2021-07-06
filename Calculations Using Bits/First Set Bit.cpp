5b7639c0877d53ccd169a4c9127c223f79efc365
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 13:05:03 +0530

    Update First Set Bit.cpp

--- Calculations Using Bits/First Set Bit.cpp
+++ Calculations Using Bits/First Set Bit.cpp
@@ -1,35 +1,34 @@
+//find the rightmost set bit occurence
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
-	int n=0,m=0;
-	while(true)
-	{
-		cout<<"enter n:";
-		cin>>n;
-		cout<<"Enter m:";
-		cin>>m;
-		int big = max(m,n);
-		int c=0;
+	//initialization
+	int n=0, c=0;
 	
-		while(m || n)
+	//Taking inputs from user
+	cout<<"enter n:";
+	cin>>n;
+	
+	//continue the loop while n>0;
+	while(n)
+	{
+		//the number isn't zero so we increament the position by 1
+		c++;
+		
+		//the right most bit is taken seperatly to check,
+		//whether that bit is set or not
+		int s = n&1;
+		
+		//if set, print the position of the set bit
+		// and break from the loop
+		if(s==1)
 		{
-			//cout<<"m:"<<m<<" n:"<<n<<endl;
-			c++;
-			if(m==n)
-			{
-				cout<<"same number"<<endl;
-				break;		
-			}
-			int s = n&1,z=m&1;
-			if(s!=z)
-			{
-				cout<<c<<endl;
-				break;
-			}
-			n=n>>1;
-			m=m>>1;
+			cout<<c<<endl;
+			break;
 		}
 		
+		// if not then the number is right shifted by 1;
+		n=n>>1;
 	}
 }
