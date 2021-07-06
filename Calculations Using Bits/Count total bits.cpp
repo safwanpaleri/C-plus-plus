c2b35fcdf19920db0aabe43e652ecef3adfdff01
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 12:52:20 +0530

    Update Count total bits.cpp

--- Calculations Using Bits/Count total bits.cpp
+++ Calculations Using Bits/Count total bits.cpp
@@ -1,29 +1,38 @@
+//For the given positive integers 'n' and 'm', find total bits after doing Binary-XOR;
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
-	int n=0,m=0;
-	while(true)
+	//initializations
+	int n=0,m=0,c=0,axb;
+	
+	//Taking Inputs from User
+	cout<<"enter n:";
+	cin>>n;
+	cout<<"Enter m:";
+	cin>>m;
+	
+	//comparing if the given numbers are same.
+	//if same there will not be any bits in common.
+	if(m==n)
+		cout<<"same number"<<endl;
+	else
 	{
-		cout<<"enter n:";
-		cin>>n;
-		cout<<"Enter m:";
-		cin>>m;
-		int big = max(m,n);
-		int c=0,axb;
-		if(m==n)
-		{
-			cout<<"same number"<<endl;
-			break;		
-		}
+		//saving the Binary-XOR result into a variable
 		axb= n^m;
+		
+		//continue the work, while the variable isn't zero
 		while(axb)
-		{			
+		{	
+			//comparison of last bits
+			//if last bit is 1 then increase count
 			if(axb&1)
 				c++;
+			//right shift by one so the next bit can be compared
 			axb=axb>>1;
 		}
-		cout<<c<<endl;
-		
+		//finally, print the count to the screen
+		cout<<"Total Bits:"<<c<<endl;
 	}
+		
 }
