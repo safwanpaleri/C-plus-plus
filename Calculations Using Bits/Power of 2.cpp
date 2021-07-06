0d12b669ef11456f85b1fc752556e336e3c386e6
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 14:05:14 +0530

    Update Power of 2.cpp

--- Calculations Using Bits/Power of 2.cpp
+++ Calculations Using Bits/Power of 2.cpp
@@ -1,15 +1,17 @@
+//Check whether the number is power of two or not
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
+	//initializations
 	int n=0;
-	while(true)
-	{
-		cout<<"enter n:";
-		cin>>n;
-		if(n&(n-1))
-			cout<<"false"<<endl;
-		else
-			cout<<"true"<<endl;
-	}
+	//Taking input from the user
+	cout<<"enter n:";
+	cin>>n;
+	
+	//The number is power of two, if the AND value with the preceeding value is zero
+	if(n&(n-1) || n==0)
+		cout<<"false"<<endl;
+	else
+		cout<<"true"<<endl;	
 }
