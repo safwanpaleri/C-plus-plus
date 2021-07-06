37b69313987e3139d7d8a206a24a71caa625134d
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 13:10:13 +0530

    Update Gray to Binary.cpp

--- Calculations Using Bits/Gray to Binary.cpp
+++ Calculations Using Bits/Gray to Binary.cpp
@@ -1,19 +1,25 @@
+//Converting Gray Code to Binary
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
-	int n=0;
-	while(true)
+	//initializations
+	int n=0,bin=0;
+	
+	//taking inputs from user
+	cout<<"enter n:";
+	cin>>n;
+	
+	//while the code is still greater than zero.
+	while(n>0)
 	{
-		cout<<"enter n:";
-		cin>>n;
-		int bin=0;
-		while(n>0)
-		{
-			n=n>>1;
-			bin = bin^n;
-		}
-		cout<<bin<<endl;
+		//the number is right shifted by one 
+		n=n>>1;
+		//the bin value is xor-ed with the current n and stored.
+		bin = bin^n;
+		//for bin to gray n^(n>>1)
 	}
-	//for bin to gray n^(n>>1)
+	//finally, after converting the binary code is printed to the screen.
+	cout<<bin<<endl;
+	
 }
