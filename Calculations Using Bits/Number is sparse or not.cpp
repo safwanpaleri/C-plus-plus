a11df41f7e4f81195eef977ea170ae909bd15b24
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 14:01:07 +0530

    Update Number is sparse or not.cpp

--- Calculations Using Bits/Number is sparse or not.cpp
+++ Calculations Using Bits/Number is sparse or not.cpp
@@ -1,17 +1,25 @@
+//Check the Number is sparse or not
+//sparse number: if no two or more consecutive bits are set in the binary representation.
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
+	//initializations
 	int n=0;
-	while(true)
-	{
-		cout<<"enter n:";
-		cin>>n;
-		int a=n&(n>>1);
-		cout<<a<<endl;
-		if(a == 0)
-			cout<<"true"<<endl;
-		else
-			cout<<"False"<<endl;
-	}
+	//taking input from user
+	cout<<"enter n:";
+	cin>>n;
+	
+	//a number is sparse if its AND value with a single right shifted value is zero
+	//If you do that the whole bits will be converted into zero if its sparse
+	
+	//so we do the calculation and store it in a variable;
+	int a=n&(n>>1);
+	
+	//checking whether the variable is zero or not
+	//if zero the number is sparse, else not
+	if(a == 0)
+		cout<<"true"<<endl;
+	else
+		cout<<"False"<<endl;
 }
