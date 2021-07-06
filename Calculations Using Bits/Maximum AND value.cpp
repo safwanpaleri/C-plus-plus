55008004f32db1964f2f5fa11a5ad20a0607f9b2
safwanpaleri <safwanpaleri@gmail.com>
2021-07-06 13:56:26 +0530

    Update Maximum AND value.cpp

--- Calculations Using Bits/Maximum AND value.cpp
+++ Calculations Using Bits/Maximum AND value.cpp
@@ -1,16 +1,18 @@
+//Find the maximum AND value of pairs in the given array
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
-	int n=4;
+	//initializations
+	int n=4,m=0;
 	int a[4]={4,8,12,16};
-	vector<int> res;
+	
+	//tranversing through all elements and performing AND operations between them
+	//and comparing the max and saving to a variable
 	for(int i=0;i<n;i++)
-	{
 		for(int j=i+1;j<n-i;j++)
-		{
-			res.push_back(a[i]&a[j]);
-		}
-	}
-	cout<<*max_element(res.begin(),res.end());
+			m=max(m,a[i]&a[j]);
+	
+	//finally, printing the value to the screen
+	cout<<m<<endl;
 }
