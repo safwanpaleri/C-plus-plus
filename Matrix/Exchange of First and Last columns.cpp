7a5809db69f69b1e95b6d4946ddcb951f5ac7880
safwanpaleri <safwanpaleri@gmail.com>
2021-07-10 19:55:53 +0530

    Update Exchange of First and Last columns.cpp

--- Matrix/Exchange of First and Last columns.cpp
+++ Matrix/Exchange of First and Last columns.cpp
@@ -1,63 +1,29 @@
 //Exchange of columns
+//swap all the first coloumn elements with last coloumn element
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
+	//initializations.
 	int n=5,m=4;
 	int mat[5][4] = { {1,2,3,4},
-					  {5,6,7,8},
-					  {9,10,11,12},
-					  {13,14,15,16},
-					  {17,18,19,20} };
-					  
-	vector<int> mv;
-	vector< vector<int> > matrix;
+			  {5,6,7,8},
+			  {9,10,11,12},
+			  {13,14,15,16},
+			  {17,18,19,20} };
+	
+	//traversing through the matrix            
 	for(int i=0;i<n;i++)
 	{
-		mv.clear();
-		for(int j=0;j<m;j++)
-			mv.push_back(mat[i][j]);
-		matrix.push_back(mv);
-	}
-    
-	cout<<"Using Array"<<endl;             
-	for(int i=0;i<n;i++)
-	{
+		//swapping first coloumn element with the last coloumn element.
 		swap(mat[i][0],mat[i][m-1]);
 	}
 	
+	//printing the matrix after exchange.
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<m;j++)
 			cout<<mat[i][j]<<" ";
 		cout<<endl;
-	}
-	
-	cout<<endl<<"using vector"<<endl;
-	n=matrix.size(),m=matrix[0].size();
-    int a[n][m];
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            a[i][j]=matrix[i][j];
-                
-    for(int i=0;i<n;i++)
-        swap(a[i][0],a[i][m-1]);
-            
-    matrix.clear();
-        
-    vector<int> av;
-    for(int i=0;i<n;i++)
-    {
-        av.clear();
-        for(int j=0;j<m;j++)
-            av.push_back(a[i][j]);
-        matrix.push_back(av);
-    }
-    
-    for(int i=0;i<n;i++)
-    {
-    	for(int j=0;j<m;j++)
-    		cout<<matrix[i][j]<<" ";
-    	cout<<endl;
 	}
 }
