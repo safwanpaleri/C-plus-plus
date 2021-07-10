d861e250d2b45e4b42487cbf4b809ce508038ccf
safwanpaleri <safwanpaleri@gmail.com>
2021-07-10 20:12:23 +0530

    Update Print in Snake Pattern.cpp

--- Matrix/Print in Snake Pattern.cpp
+++ Matrix/Print in Snake Pattern.cpp
@@ -1,56 +1,33 @@
-//print matrice in snake pattern
+//print matrix in snake pattern
 #include<bits/stdc++.h>
 using namespace std;
-void usingVector(vector< vector<int> > &A)
-{
-	for(int i=0;i<A.size();i++)
-	{
-		if(i%2 ==0)
-		{
-			for(int j=0;j<A[0].size();j++)
-				cout<<A[i][j]<<" ";
-		}
-		else
-		{
-			for(int j=A[0].size()-1;j>=0;j--)
-				cout<<A[i][j]<<" ";
-		}
-	}
-}
 main()
 {
-	int mat[4][4] = { {10, 20, 30, 40},
-                   {15, 25, 35, 45},
-                   {27, 29, 37, 48},
-                   {32, 33, 39, 50}};
-                   
-    int n=4;
-	cout<<"Using Array"<<endl;      
-    for(int i=0;i<n;i++)
-    {
-    	if(i%2 == 0)
-    	{
-    		for(int j=0;j<n;j++)
-    			cout<<mat[i][j]<<" ";
-		}
-		else
+	//initializations.
+	int n=4;
+	int mat[n][n] = { {10, 20, 30, 40},
+                   	  {15, 25, 35, 45},
+                   	  {27, 29, 37, 48},
+                   	  {32, 33, 39, 50}  };
+	
+	//the process is exactly same for vectors too.
+	
+	//transverse through the matrix. and print alternative rows in snake pattern
+	for(int i=0;i<n;i++)
+	{
+		//if the current row order is even
+		if(i%2 == 0)
+	    	{
+			//then print elements of row from left to right
+			for(int j=0;j<n;j++)
+				cout<<mat[i][j]<<" ";
+	    	}
+		//if the current row order is odd
+	    	else
 		{
+			//then print elements of row from right to left.
 			for(int j=n-1;j>=0;j--)
 				cout<<mat[i][j]<<" ";
-		}
-	}
-    
-    cout<<endl;
-    cout<<"Using Vector"<<endl;
-    vector<int> a;
-    vector< vector<int> >A;
-    for(int i=0;i<n;i++)
-    {
-    	a.clear();
-    	for(int j=0;j<n;j++)
-    		a.push_back(mat[i][j]);
-    	A.push_back(a);
-	}
-	usingVector(A);
-	
+	    	}
+    	}
 }
