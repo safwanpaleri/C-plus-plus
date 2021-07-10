2740af299246cdb0eeb06546fc2647cabf21802d
safwanpaleri <safwanpaleri@gmail.com>
2021-07-10 20:21:17 +0530

    Update Rotate Matrix by 90 Degree.cpp

--- Matrix/Rotate Matrix by 90 Degree.cpp
+++ Matrix/Rotate Matrix by 90 Degree.cpp
@@ -1,67 +1,35 @@
-//Rotate Matrix by 90
+//Rotate Matrix by 90 degree
 #include<bits/stdc++.h>
 using namespace std;
-void usingVector(vector< vector<int> > &A)
-{
-	int m=A.size(), n=A[0].size();
-    int a[m][n];
-    for(int i=0;i<m;i++)
-        for(int j=0;j<n;j++)
-            a[i][j]=A[i][j];
-        
-    for(int i=0;i<m;i++)
-    	for(int j=i+1;j<n;j++)
-    		swap(a[i][j],a[j][i]);
-    		    
-    for (int i = 0; i < n; i++)
-        for (int j = 0, k = n - 1; j < k; j++, k--)
-            swap(a[j][i], a[k][i]);
-            
-    for(int i=0;i<n;i++)
-    {
-    	for(int j=0;j<n;j++)
-    		cout<<a[i][j]<<" ";
-    	cout<<endl;
-	}
-}
 main()
 {
+	//initializations
 	int n=4;
-	int mat[4][4] = { {1,2,3,4},
+	int mat[n][n] = { {1,2,3,4},
                    	  {5,6,7,8},
-                      {9,10,11,12},
-                      {13,14,15,16} };
-    
-	cout<<"Using Vector"<<endl;                 
-    vector<int> a;
-    vector< vector<int> >A;
-    for(int i=0;i<n;i++)
-    {
-    	a.clear();
-    	for(int j=0;j<n;j++)
-    		a.push_back(mat[i][j]);
-    	A.push_back(a);
-	}
-	usingVector(A);
-    
-	cout<<endl;               
-
+                          {9,10,11,12},
+                          {13,14,15,16} };
+     	
+	//the process is exactly same for vectors too.
 	
+	//we rotate the matrix in 90 by, reversing the transpose of given matrix.
 	
-	cout<<"Using Array"<<endl;      
-    for(int i=0;i<n;i++)
-    	for(int j=i+1;j<n;j++)
-    		swap(mat[i][j],mat[j][i]);
-    		
-    for (int i = 0; i < n; i++)
-        for (int j = 0, k = n - 1; j < k; j++, k--)
-            swap(mat[j][i], mat[k][i]);
-    
-    for(int i=0;i<n;i++)
-    {
-    	for(int j=0;j<n;j++)
-    		cout<<mat[i][j]<<" ";
-    	cout<<endl;
+	//finding transpose of the given matrix.
+    	for(int i=0;i<n;i++)
+    		for(int j=i+1;j<n;j++)
+    			swap(mat[i][j],mat[j][i]);
+    	
+	//the reversing the transpose matrix.
+    	for (int i = 0; i < n; i++)
+        	for (int j = 0, k = n - 1; j < k; j++, k--)
+            		swap(mat[j][i], mat[k][i]);
+    	
+	//printing the matrix after rotated by 90.
+    	for(int i=0;i<n;i++)
+    	{
+    		for(int j=0;j<n;j++)
+    			cout<<mat[i][j]<<" ";
+    		cout<<endl;
 	}
     
 }
