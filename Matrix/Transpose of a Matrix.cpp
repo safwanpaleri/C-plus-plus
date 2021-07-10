80ffb173d4ae83377a0d88f55dfd69a6296902de
safwanpaleri <safwanpaleri@gmail.com>
2021-07-10 20:45:23 +0530

    Update Transpose of a Matrix.cpp

--- Matrix/Transpose of a Matrix.cpp
+++ Matrix/Transpose of a Matrix.cpp
@@ -1,56 +1,25 @@
 //Transpose of a matrix
 #include<bits/stdc++.h>
 using namespace std;
-void usingVector(vector< vector<int> > &A)
-{
-	int m=A.size(), n=A[0].size();
-    int a[m][n];
-    for(int i=0;i<m;i++)
-        for(int j=0;j<n;j++)
-            a[i][j]=A[i][j];
-        
-        for(int i=0;i<m;i++)
-    	    for(int j=i+1;j<n;j++)
-    		    swap(a[i][j],a[j][i]);
-    		    
-    for(int i=0;i<n;i++)
-    {
-    	for(int j=0;j<n;j++)
-    		cout<<a[i][j]<<" ";
-    	cout<<endl;
-	}
-}
 main()
 {
+	//initializations
 	int n=3;
-	int mat[3][3] = { {1,2,3},
+	int mat[n][n] = { {1,2,3},
                    	  {4,5,6},
-                      {7,8,9} };
-    
-	cout<<"Using Vector"<<endl;                 
-    vector<int> a;
-    vector< vector<int> >A;
-    for(int i=0;i<n;i++)
-    {
-    	a.clear();
-    	for(int j=0;j<n;j++)
-    		a.push_back(mat[i][j]);
-    	A.push_back(a);
-	}
-	usingVector(A);
-    
-	cout<<endl;               
+                      	  {7,8,9} };              
 
-	cout<<"Using Array"<<endl;      
-    for(int i=0;i<n;i++)
-    	for(int j=i+1;j<n;j++)
-    		swap(mat[i][j],mat[j][i]);
-    
-    for(int i=0;i<n;i++)
-    {
-    	for(int j=0;j<n;j++)
-    		cout<<mat[i][j]<<" ";
-    	cout<<endl;
+    	//traversing through matrix and swapping the numbers.
+    	for(int i=0;i<n;i++)
+    		for(int j=i+1;j<n;j++)
+    			swap(mat[i][j],mat[j][i]);
+	
+	//printing after transposing.
+    	for(int i=0;i<n;i++)
+    	{
+    		for(int j=0;j<n;j++)
+    			cout<<mat[i][j]<<" ";
+    		cout<<endl;
 	}
     
 }
